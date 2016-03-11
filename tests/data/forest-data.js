79bdc8333e7506c1ca89754c0287ba8834c12fdf
陈铖 <azureternite@live.com>
2016-03-12 06:02:38 +0800

    update circle effects

---
+++ tests/data/forest-data.js
@@ -0,0 +1,187 @@
+var plantsData = [{
+	name: 'Amazon',
+	coords: [10.0091294, -70.0371812],
+	status: 'closed',
+	offsets: [0, 2]
+}, {
+	name: 'Siberian taiga',
+	coords: [64.0543102, 115.4825862],
+	status: 'closed',
+	offsets: [0, 2]
+}, {
+	name: 'Siberian taiga',
+	coords: [67.5543102, 96.4825862],
+	status: 'closed',
+	offsets: [0, 2]
+}, {
+	name: 'Siberian taiga',
+	coords: [60.5543102, 93.4825862],
+	status: 'closed',
+	offsets: [0, 2]
+}, {
+	name: 'Siberian taiga',
+	coords: [61.0543102, 65.4825862],
+	status: 'closed',
+	offsets: [0, 2]
+}, {
+	name: 'Siberian taiga',
+	coords: [66.0543102, 136.4825862],
+	status: 'closed',
+	offsets: [0, 2]
+}, {
+	name: '青木原树海6',
+	coords: [36.9030599, 138.4213693],
+	status: 'closed'
+}, {
+	name: '青木原树海5',
+	coords: [-34.9030599, 142.4213693],
+	status: 'closed'
+}, {
+	name: '青木原树海1',
+	coords: [14.9030599, 108.4213693],
+	status: 'closed'
+}, {
+	name: '青木原树海2',
+	coords: [18.9030599, 100.6213693],
+	status: 'closed'
+}, {
+	name: '青木原树海3',
+	coords: [14.9030599, 98.6213693],
+	status: 'closed'
+}, {
+	name: '青木原树海4',
+	coords: [-4.3030599, 139.4213693],
+	status: 'closed'
+}, {
+	name: 'Congo\'s rainforest1',
+	coords: [1.4472465, 22.9903674],
+	status: 'closed'
+}, {
+	name: 'Congo\'s rainforest2',
+	coords: [-6.4472465, 16.9903674],
+	status: 'closed'
+}, {
+	name: 'Congo\'s rainforest3',
+	coords: [-6.7472465, 36.9903674],
+	status: 'closed'
+}, {
+	name: 'Congo\'s rainforest4',
+	coords: [5.4472465, 24.9903674],
+	status: 'closed'
+}, {
+	name: 'Norway forest',
+	coords: [60.513264, 7.1020357],
+	status: 'activeUntil2018'
+}, {
+	name: 'jingpo Lake',
+	coords: [44.364503, 129.076252],
+	status: 'closed'
+}, {
+	name: 'Vienna woods',
+	coords: [48.0331853, 15.0505223],
+	status: 'closed',
+	offsets: [0, -2]
+}, {
+	name: 'Swedish forest',
+	coords: [59.1351446, 18.1348691],
+	status: 'closed',
+	offsets: [0, -2]
+}, {
+	name: 'Finnish forest',
+	coords: [60.6200685, 24.5306289],
+	status: 'closed'
+}, {
+	name: 'Italian forest',
+	coords: [45.6558015, 12.2500848],
+	status: 'closed',
+	offsets: [0, -2]
+}, {
+	name: 'Canada forest',
+	coords: [43.1417497, 79.6583877],
+	status: 'closed'
+}, {
+	name: 'Canada forest',
+	coords: [45.709331, 73.415865],
+	status: 'closed'
+}, {
+	name: 'Canada forest',
+	coords: [49.6396481, 123.3915617],
+	status: 'closed'
+}, {
+	name: 'Chilean forests',
+	coords: [-33.0401151, -70.3721088],
+	status: 'activeUntil2022'
+}, {
+	name: 'Chilean forests',
+	coords: [-23.0401151, -70.3721088],
+	status: 'activeUntil2022'
+}, {
+	name: 'Chilean forests',
+	coords: [-11.0401151, -71.3721088],
+	status: 'activeUntil2022'
+}, {
+	name: 'KKB',
+	coords: [43.8913533, 29.2005777],
+	status: 'closed',
+	offsets: [0, -5]
+}, {
+	name: 'KKI',
+	coords: [23.5544748, 12.3472095],
+	status: 'activeUntil2022',
+	offsets: [0, 2]
+}, {
+	name: 'KKU',
+	coords: [63.4293465, -128.4774649],
+	status: 'closed'
+}, {
+	name: 'KNK',
+	coords: [19.1473279, 78.3827739],
+	status: 'closed'
+}, {
+	name: 'KKP',
+	coords: [69.2513078, 58.4356761],
+	status: 'activeUntil2022',
+	offsets: [0, -2]
+}, {
+	name: 'KKG3',
+	coords: [49.9841308, -110.1846373],
+	status: 'activeUntil2018'
+}, {
+	name: 'KKG2',
+	coords: [32.9841308, -90.1846373],
+	status: 'activeUntil2018'
+}, {
+	name: 'KKG1',
+	coords: [69.9841308, -160.1846373],
+	status: 'activeUntil2018'
+}, {
+	name: 'KKG4',
+	coords: [19.9841308, -102.1846373],
+	status: 'activeUntil2018'
+}, {
+	name: 'KKK',
+	coords: [53.4104656, 10.4091597],
+	status: 'closed'
+}, {
+	name: 'KWG',
+	coords: [32.0348748, 109.4097793],
+	status: 'activeUntil2022'
+}, {
+	name: 'KBR',
+	coords: [53.850666, 39.3457603],
+	status: 'closed',
+	offsets: [0, 5]
+}, {
+	name: 'KMK',
+	coords: [-50.408791, 97.4861956],
+	status: 'closed'
+}, {
+	name: 'THTR',
+	coords: [21.6786228, -7.9700232],
+	status: 'closed'
+}, {
+	name: 'KKE',
+	coords: [-72.4216974, 7.3706389],
+	status: 'activeUntil2022',
+	offsets: [0, 2]
+}]
